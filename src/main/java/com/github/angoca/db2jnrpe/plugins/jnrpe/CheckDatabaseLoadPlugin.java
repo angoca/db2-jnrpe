e5af3c116d714d2466f108710ae93d65158d133e
Andres Gomez Casanova <angoca@yahoo.com>
2014-11-26 18:46:53 +0100

    Tester

--- src/main/java/com/github/angoca/db2jnrpe/plugins/jnrpe/CheckDatabaseLoadPlugin.java
+++ src/main/java/com/github/angoca/db2jnrpe/plugins/jnrpe/CheckDatabaseLoadPlugin.java
@@ -47,9 +47,102 @@ public final class CheckDatabaseLoadPlugin extends AbstractDB2PluginBase {
      */
     private static final String WARNING_VALUE = "7000";
 
+    /**
+     * Tests the complete chain.
+     *
+     * @param args
+     *            Nothing.
+     * @throws Exception
+     *             If any error occur.
+     */
+    public static void main(final String[] args) throws Exception {
+        // CHECKSTYLE:OFF
+        ICommandLine cl = new ICommandLine() {
+
+            @Override
+            public String getOptionValue(final String optionName) {
+                String ret = null;
+                if (optionName.equals("hostname")) {
+                    ret = "localhost";
+                } else if (optionName.equals("port")) {
+                    ret = "50000";
+                } else if (optionName.equals("database")) {
+                    ret = "sample";
+                } else if (optionName.equals("username")) {
+                    ret = "db2inst1";
+                } else if (optionName.equals("password")) {
+                    ret = "db2inst1";
+                }
+                return ret;
+            }
+
+            @Override
+            public List<String> getOptionValues(final String optionName) {
+                return null;
+            }
+
+            @Override
+            public String getOptionValue(final String optionName,
+                    final String defaultValue) {
+                return defaultValue;
+            }
+
+            @Override
+            public String getOptionValue(final char shortOptionName) {
+                return null;
+            }
+
+            @Override
+            public List<String> getOptionValues(final char shortOptionName) {
+                return null;
+            }
+
+            @Override
+            public String getOptionValue(final char shortOptionName,
+                    final String defaultValue) {
+                return null;
+            }
+
+            @Override
+            public boolean hasOption(final String optionName) {
+                return false;
+            }
+
+            @Override
+            public boolean hasOption(final char shortOptionName) {
+                return false;
+            }
+
+        };
+        ThresholdsEvaluatorBuilder thrb = new ThresholdsEvaluatorBuilder();
+        Collection<Metric> c;
+        AbstractDB2PluginBase p;
+        p = new CheckDatabaseLoadPlugin();
+        p.configureThresholdEvaluatorBuilder(thrb, cl);
+        try {
+            p.gatherMetrics(cl);
+        } catch (final Exception e) {
+            System.out.println("First snapshot");
+        }
+        Thread.sleep(2000);
+        try {
+            c = p.gatherMetrics(cl);
+            System.out.println(c.toString() + ':' + c.size());
+        } catch (final Exception e) {
+            System.out.println("Second snapshot");
+        }
+        Thread.sleep(5000);
+        c = p.gatherMetrics(cl);
+        System.out.println(c.toString() + ':' + c.size());
+        Thread.sleep(5000);
+        c = p.gatherMetrics(cl);
+        System.out.println(c.toString() + ':' + c.size());
+        // CHECKSTYLE:ON
+    }
+
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see
      * it.jnrpe.plugins.PluginBase#configureThresholdEvaluatorBuilder(it.jnrpe
      * .utils.thresholds.ThresholdsEvaluatorBuilder, it.jnrpe.ICommandLine)
@@ -57,7 +150,7 @@ public final class CheckDatabaseLoadPlugin extends AbstractDB2PluginBase {
     @Override
     public void configureThresholdEvaluatorBuilder(
             final ThresholdsEvaluatorBuilder thrb, final ICommandLine cl)
-                    throws BadThresholdException {
+            throws BadThresholdException {
         final String dbId = this.getId(cl);
         this.log.warn("Database: " + dbId);
         thrb.withLegacyThreshold(CheckDatabaseLoadPlugin.UID_LOAD, null, cl
@@ -86,7 +179,7 @@ public final class CheckDatabaseLoadPlugin extends AbstractDB2PluginBase {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see it.jnrpe.plugins.PluginBase#gatherMetrics(it.jnrpe.ICommandLine)
      */
     @Override
@@ -139,7 +232,7 @@ public final class CheckDatabaseLoadPlugin extends AbstractDB2PluginBase {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see it.jnrpe.plugins.PluginBase#getPluginName()
      */
     @Override
